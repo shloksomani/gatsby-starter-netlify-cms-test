0855804d4804a4436cd7d2016643992dd98eb8b5
shloksomani <shloksomani@ymail.com>
2020-05-02 16:43:33 -0400

    removed all the clutter

--- src/cms/preview-templates/IndexPagePreview.js
+++ src/cms/preview-templates/IndexPagePreview.js
@@ -1,32 +1,18 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { IndexPageTemplate } from '../../templates/index-page'
+import React from "react";
+import PropTypes from "prop-types";
+import IndexPage from "../../templates/index-page";
 
 const IndexPagePreview = ({ entry, getAsset }) => {
-  const data = entry.getIn(['data']).toJS()
+  // const data = entry.getIn(["data"]).toJS();
 
-  if (data) {
-    return (
-      <IndexPageTemplate
-        image={getAsset(data.image)}
-        title={data.title}
-        heading={data.heading}
-        subheading={data.subheading}
-        description={data.description}
-        intro={data.intro || { blurbs: [] }}
-        mainpitch={data.mainpitch || {}}
-      />
-    )
-  } else {
-    return <div>Loading...</div>
-  }
-}
+  return <IndexPage />;
+};
 
 IndexPagePreview.propTypes = {
   entry: PropTypes.shape({
     getIn: PropTypes.func,
   }),
   getAsset: PropTypes.func,
-}
+};
 
-export default IndexPagePreview
+export default IndexPagePreview;
---
+++ src/pages/idk.js
@@ -0,0 +1,13 @@
+// import React from "react";
+// import { Link } from "gatsby";
+
+// export default function Home() {
+//   return (
+//     <main>
+//       <h1>What a world.</h1>
+//       <p>
+//         <Link to="/contact/">Contact</Link>
+//       </p>
+//     </main>
+//   );
+// }
